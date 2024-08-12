fe4c944938ae3bbb627d77c181d9aafb12239659
Albert <albert.bernadas.fabra@gmail.com>
2024-08-12 12:22:17 +0200

    Ajuste de los modelos de categoría, subcategoria y recompensa a los estándares

--- CrowdSisters/Models/Categoria.cs
+++ CrowdSisters/Models/Categoria.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace CrowdSisters.Models
 {
+    [Table("Categoria")]
     public class Categoria
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IDCategoria { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Nombre { get; set; }
 
-        // Navigation property
         public ICollection<Subcategoria> Subcategorias { get; set; }
     }
 }
