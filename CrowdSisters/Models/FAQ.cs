0e223e22539e13cd9c2f0a98b0825c54a08ca3ed
Marc Marimon <crammarimon@hotmail.com>
2024-08-12 11:05:43 +0200

    Modelos Actualizacion, FAQ y Proyecto hechos

---
+++ CrowdSisters/Models/FAQ.cs
@@ -0,0 +1,14 @@
+﻿namespace CrowdSisters.Models
+{
+    public class FAQ
+    {
+        public int IDComentario { get; set; }
+        public int FKProyecto { get; set; }
+        public string Contenido { get; set; }
+        public DateTime FechaComentario { get; set; }
+        public string Titulo { get; set; }
+
+        // Navigation property
+        public Proyecto Proyecto { get; set; }
+    }
+}
