a1ed357a00f52c20de1029c2601aabc7ba99b2fb
Adria Tomas <adriatomasmorancho@gmail.com>
2024-08-19 12:22:04 +0200

    Pagina Crear Proyecto,view, controller, cshtml

---
+++ CrowdSisters/Controllers/CrearProyectoController.cs
@@ -0,0 +1,55 @@
+﻿using CrowdSisters.Models;
+using CrowdSisters.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrowdSisters.Controllers
+{
+    public class CrearProyectoController : Controller
+    {
+        private readonly ServiceCrearProyecto _serviceCrearProyecto;
+
+
+        public CrearProyectoController(ServiceCrearProyecto serviceCrearProyecto)
+        {
+            _serviceCrearProyecto = serviceCrearProyecto;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            /*Comprobar si algun usuario tiene iniciada la session, si no la tiene redireccion directa al Login*/
+
+            /*Sacar toda la información del usuario que tiene iniciada la sessión*/
+
+            ViewBag.Usuario = await _serviceCrearProyecto.CrearProjecteView();
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(CrearProyectoViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            /*Subir las fotos a Firebase i sacar las Urls*/
+
+            /*Hacer un update dek usuario*/
+
+            /* Creación del proyecto*/
+
+            await _serviceCrearProyecto.CreateProyectoAsync(model.Proyecto);
+
+            return RedirectToAction("Index", "Proyecto");
+        }
+
+
+
+
+
+       
+
+
+    }
+}
---
+++ CrowdSisters/Models/CrearProyectoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CrowdSisters.Models
+{
+    public class CrearProyectoViewModel
+    {
+        public Usuario Usuario { get; set; } = new Usuario();
+        public Proyecto Proyecto { get; set; } = new Proyecto();
+  
+    }
+}
